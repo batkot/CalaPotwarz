efc90c6ab19bcb952274090b7ee7bdf56ad72128
Łukasz Płaziak <lukasz.plaziak@mp.pl>
2015-09-05 14:40:20 +0200

    domino piece placer - checks if domino piece can get placed in given field on board

--- TWFHackathon/App/Scripts/app/Services/PiecePlacer.js
+++ TWFHackathon/App/Scripts/app/Services/PiecePlacer.js
@@ -5,16 +5,64 @@ var Services;
         }
         DominoPiecePlacer.prototype.place = function (board, piece, firstTileX, firstTileY) {
             var firstTileOnBoard = board.getTile(firstTileX, firstTileY);
-            if (firstTileOnBoard != null && firstTileOnBoard != undefined) {
+            if (firstTileOnBoard != null) {
                 return false;
             }
             var secondTileX = piece.orientation == Models.DominoPieceOrientation.LeftRight ? firstTileX + 1 : firstTileX;
             var secondTileY = piece.orientation == Models.DominoPieceOrientation.LeftRight ? firstTileY : firstTileY + 1;
             var secondTileOnBoard = board.getTile(secondTileX, secondTileY);
-            if (firstTileOnBoard != null && firstTileOnBoard != undefined) {
+            if (secondTileOnBoard != null) {
+                return false;
+            }
+            var firstTileNeighbours = this.getFirstTileNeighboursCoordinates(piece, firstTileX, firstTileY);
+            if (!this.checkTileNeighbours(board, piece.firstTile, firstTileNeighbours)) {
+                return false;
+            }
+            var secondTileNeighbours = this.getSecondTileNeighboursCoordinates(piece, secondTileX, secondTileY);
+            if (!this.checkTileNeighbours(board, piece.secondTile, secondTileNeighbours)) {
                 return false;
             }
             return true;
+        };
+        DominoPiecePlacer.prototype.checkTileNeighbours = function (board, tile, neighbours) {
+            for (var tileXY in neighbours) {
+                var tileOnBoard = board.getTile(tileXY.x, tileXY.y);
+                if (tileOnBoard == null) {
+                    continue;
+                }
+                else if (tileOnBoard.category != tile.category) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        DominoPiecePlacer.prototype.getFirstTileNeighboursCoordinates = function (piece, x, y) {
+            var firstTileNeighbours;
+            if (piece.orientation == Models.DominoPieceOrientation.LeftRight) {
+                firstTileNeighbours[0] = { x: x - 1, y: y };
+                firstTileNeighbours[1] = { x: x, y: y - 1 };
+                firstTileNeighbours[2] = { x: x, y: y + 1 };
+            }
+            else {
+                firstTileNeighbours[0] = { x: x, y: y - 1 };
+                firstTileNeighbours[1] = { x: x - 1, y: y };
+                firstTileNeighbours[2] = { x: x + 1, y: y };
+            }
+            return firstTileNeighbours;
+        };
+        DominoPiecePlacer.prototype.getSecondTileNeighboursCoordinates = function (piece, x, y) {
+            var secondTileNeighbours;
+            if (piece.orientation == Models.DominoPieceOrientation.LeftRight) {
+                secondTileNeighbours[0] = { x: x + 1, y: y };
+                secondTileNeighbours[1] = { x: x, y: y - 1 };
+                secondTileNeighbours[2] = { x: x, y: y + 1 };
+            }
+            else {
+                secondTileNeighbours[0] = { x: x, y: y + 1 };
+                secondTileNeighbours[1] = { x: x - 1, y: y };
+                secondTileNeighbours[2] = { x: x + 1, y: y };
+            }
+            return secondTileNeighbours;
         };
         return DominoPiecePlacer;
     })();
