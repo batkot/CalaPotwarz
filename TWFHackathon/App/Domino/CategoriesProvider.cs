3fac0e748764cca93cb9cc0159e177f75429682e
Tomasz Batko <tomasz.batko@mp.pl>
2015-09-05 12:02:14 +0200

    ICanCreateGame introduced

---
+++ TWFHackathon/App/Domino/CategoriesProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Domino
+{
+    public class CategoriesProvider
+    {
+        private List<string> _categories;
+
+        public CategoriesProvider(List<string> categories)
+        {
+            _categories = categories;
+        }
+        public string DrawCategory()
+        {
+            return _categories[_rand.Next(_categories.Count)];
+        }
+
+        private static Random _rand = new Random(Guid.NewGuid().GetHashCode());
+    }
+}
--- TWFHackathon/App/Domino/ICanCreateGame.cs
+++ TWFHackathon/App/Domino/ICanCreateGame.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using App.Models;
+﻿using App.Models;
 
 namespace App.Domino
 {
     public interface ICanCreateGame
     {
         GameModel Create();
-    }
-
-    public class DefaultGameCreator : ICanCreateGame
-    {
-        public GameModel Create()
-        {
-            return new GameModel(Guid.NewGuid().ToString(),
-                4,
-                4,
-                new DominoTile("Animal"),
-                new DominoTile("Zavoir"),
-                new DominoPiece(new DominoTile("Zavoir"), new DominoTile("Animal")),
-                new DominoPiece(new DominoTile("Animal"), new DominoTile("Letter")),
-                new DominoPiece(new DominoTile("Zavoir"), new DominoTile("Letter")),
-                new DominoPiece(new DominoTile("People"), new DominoTile("Shoe")));
-        }
     }
 }
