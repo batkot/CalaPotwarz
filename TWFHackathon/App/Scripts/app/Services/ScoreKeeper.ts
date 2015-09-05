7e249c3e89795629e91fbe807a175728d931f918
Łukasz Płaziak <lukasz.plaziak@mp.pl>
2015-09-05 19:19:29 +0200

    computing score

--- TWFHackathon/App/Scripts/app/Services/ScoreKeeper.ts
+++ TWFHackathon/App/Scripts/app/Services/ScoreKeeper.ts
@@ -5,5 +5,26 @@
         public addPieceTaken(): void {
             (this._piecesTakenCount)++;
         }
+
+        public ComputeScore(game: Models.Game, board: Models.Board): number {
+
+            var placedPiecesCount: number = board.PlacedPieces != null ? board.PlacedPieces.length : 0;
+            var playerPiecesCount: number = game.PlayerPieces != null ? game.PlayerPieces.length : 0;
+
+            var score: number = 5;
+
+            if (placedPiecesCount < playerPiecesCount)
+                score = 5;
+            else if (placedPiecesCount == playerPiecesCount)
+                score = 4
+            else if (placedPiecesCount > playerPiecesCount)
+                score = 3;
+
+            if (this._piecesTakenCount > (placedPiecesCount / 2)){
+                score -= 1;
+            }
+
+            return score;
+        }
     }
 }
