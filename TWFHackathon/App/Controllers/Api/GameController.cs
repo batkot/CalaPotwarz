58acce1033d7454cb02291310b33cd9bd5f6725d
Tomasz Batko <tomasz.batko@mp.pl>
2015-09-05 10:04:58 +0200

    Ninject IoC

---
+++ TWFHackathon/App/Controllers/Api/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+
+namespace App.Controllers.Api
+{
+    public class GameController : ApiController
+    {
+        private readonly IFoo _foo;
+        public GameController(IFoo foo)
+        {
+            _foo = foo;
+        }
+
+        public string Get()
+        {
+            return _foo.DoStuff();
+        }
+    }
+
+    public interface IFoo
+    {
+        string DoStuff();
+    }
+
+    public class Bar : IFoo
+    {
+        public string DoStuff()
+        {
+            return "Yo";
+        }
+    }
+}
