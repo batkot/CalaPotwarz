7777c7a1233975c3eded7f70b1b1081d2702436a
Łukasz Płaziak <lukasz.plaziak@mp.pl>
2015-09-05 18:38:22 +0200

    fixed piece placer

--- TWFHackathon/App/Scripts/app/Models/Game.ts
+++ TWFHackathon/App/Scripts/app/Models/Game.ts
@@ -53,7 +53,7 @@
 
         public getTile(x: number, y: number): DominoTile{
             var cell = this.Cells[x][y];
-            if (cell.isEmpty())
+            if (cell == null || cell == undefined || cell.isEmpty())
                 return null;
             else
                 return cell.DominoTile;
