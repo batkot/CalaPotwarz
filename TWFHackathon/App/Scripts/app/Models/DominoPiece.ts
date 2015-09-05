d7edf59d3f6ecd8c8d98550b1492289395610c16
Łukasz Płaziak <lukasz.plaziak@mp.pl>
2015-09-05 13:46:30 +0200

    models in one place

--- TWFHackathon/App/Scripts/app/Models/DominoPiece.ts
+++ TWFHackathon/App/Scripts/app/Models/DominoPiece.ts
@@ -1,35 +1,3 @@
 ﻿module Models {
-    export class DominoPiece {
-        constructor(private _tiles: DominoTile[], private _orientation: DominoPieceOrientation) { };
-
-        get firstTile() : DominoTile {
-            return this._tiles[0];
-        }
-
-        get secondTile() : DominoTile {
-            return this._tiles[1];
-        }
-
-        get orientation() : DominoPieceOrientation {
-            return this._orientation;
-        }
-    }
-
-    export class DominoTile {
-        constructor(private _category: string, private _imageUrl: string) {
-        }
-
-        get category() : string {
-            return this._category;
-        }
-
-        get imageUrl() : string {
-            return this._imageUrl;
-        }
-    }
-
-    export enum DominoPieceOrientation {
-        UpDown,
-        LeftRight,
-    }
+    
 }
--- TWFHackathon/App/Scripts/app/Models/Game.js
+++ TWFHackathon/App/Scripts/app/Models/Game.js
@@ -2,7 +2,8 @@ var Models;
 (function (Models) {
     'use strict';
     var Game = (function () {
-        function Game(Id, Name, Height, Width) {
+        function Game(PlayerPieces, Id, Name, Height, Width) {
+            this.PlayerPieces = PlayerPieces;
             this.Id = Id;
             this.Name = Name;
             this.Height = Height;
@@ -15,7 +16,7 @@ var Models;
         function Board(_height, _width) {
             this._height = _height;
             this._width = _width;
-            this._fields = new Models.DominoTile[_width, _height];
+            this._fields = new DominoTile[_width, _height];
         }
         Board.prototype.getTile = function (x, y) {
             return this._fields[x][y];
@@ -23,4 +24,62 @@ var Models;
         return Board;
     })();
     Models.Board = Board;
+    var DominoPiece = (function () {
+        function DominoPiece(_tiles, _orientation) {
+            this._tiles = _tiles;
+            this._orientation = _orientation;
+        }
+        ;
+        Object.defineProperty(DominoPiece.prototype, "firstTile", {
+            get: function () {
+                return this._tiles[0];
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(DominoPiece.prototype, "secondTile", {
+            get: function () {
+                return this._tiles[1];
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(DominoPiece.prototype, "orientation", {
+            get: function () {
+                return this._orientation;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return DominoPiece;
+    })();
+    Models.DominoPiece = DominoPiece;
+    var DominoTile = (function () {
+        function DominoTile(_category, _imageUrl) {
+            this._category = _category;
+            this._imageUrl = _imageUrl;
+        }
+        Object.defineProperty(DominoTile.prototype, "category", {
+            get: function () {
+                return this._category;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(DominoTile.prototype, "imageUrl", {
+            get: function () {
+                return this._imageUrl;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return DominoTile;
+    })();
+    Models.DominoTile = DominoTile;
+    (function (DominoPieceOrientation) {
+        DominoPieceOrientation[DominoPieceOrientation["UpDown"] = 0] = "UpDown";
+        DominoPieceOrientation[DominoPieceOrientation["LeftRight"] = 1] = "LeftRight";
+    })(Models.DominoPieceOrientation || (Models.DominoPieceOrientation = {}));
+    var DominoPieceOrientation = Models.DominoPieceOrientation;
 })(Models || (Models = {}));
+//# sourceMappingURL=Game.js.map
