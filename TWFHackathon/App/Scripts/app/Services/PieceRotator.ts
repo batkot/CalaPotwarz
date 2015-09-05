40bf5f26bea44ac47d2769a6655eaeaf081170eb
Łukasz Płaziak <lukasz.plaziak@mp.pl>
2015-09-05 15:24:27 +0200

    domino piece rotator

---
+++ TWFHackathon/App/Scripts/app/Services/PieceRotator.ts
@@ -0,0 +1,40 @@
+﻿module Services {
+    export interface IDominoPieceRotator {
+        rotateRight(piece: Models.DominoPiece): Models.DominoPiece;
+        rotateLeft(piece: Models.DominoPiece): Models.DominoPiece;
+    }
+
+    export class DominoPieceRotator implements IDominoPieceRotator {
+
+        public rotateRight(piece: Models.DominoPiece): Models.DominoPiece {
+            if (piece.orientation == Models.DominoPieceOrientation.UpDown) {
+                return this.getRotatedPiece(piece, true);
+            }
+            else {
+                return this.getRotatedPiece(piece, false);
+            }
+        }
+
+        public rotateLeft(piece: Models.DominoPiece): Models.DominoPiece {
+            if (piece.orientation == Models.DominoPieceOrientation.UpDown) {
+                return this.getRotatedPiece(piece, false);
+            }
+            else {
+                return this.getRotatedPiece(piece, true);
+            }
+        }
+
+        private getRotatedPiece(piece: Models.DominoPiece, changeTileOrder: boolean): Models.DominoPiece {
+
+            var newOrientation: Models.DominoPieceOrientation;
+            newOrientation = piece.orientation == Models.DominoPieceOrientation.UpDown
+                ? Models.DominoPieceOrientation.LeftRight
+                : Models.DominoPieceOrientation.UpDown;
+
+            var firstTile: Models.DominoTile = changeTileOrder ? piece.secondTile : piece.firstTile;
+            var secondTile: Models.DominoTile = changeTileOrder ? piece.firstTile : piece.secondTile;
+
+            return new Models.DominoPiece(firstTile, secondTile, newOrientation);
+        }
+    }
+}
